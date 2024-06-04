1705f31baf8a3251d92f854673a83965c5556967
Niccolas Maganeli <niccomaganeli@hotmail.com>
2024-06-03 23:44:38 -0300

    base config + sleuth

--- lua/plugins/base.lua
+++ lua/plugins/base.lua
@@ -1,6 +1,52 @@
+-- NOTE: to understand what each configuration is doing here are some useful commands
+-- :help vim.opt
+-- :help option-list (this gives a whole list which is useful to search for options)
 vim.opt.relativenumber = true
 vim.opt.mouse = "a" -- Usefol for resizing splits
 vim.opt.showmode = false
+vim.opt.clipboard = "unnamedplus"
+vim.opt.breakindent = true
+vim.opt.undofile = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.updatetime = 250
+vim.opt.timeoutlen = 300
+vim.opt.signcolumn = "yes"
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+vim.opt.list = true
+vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+vim.opt.inccommand = "split"
+vim.opt.cursorline = true
+vim.opt.scrolloff = 10
+vim.opt.hlsearch = true
+
+-- Keymaps
+vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
+
+vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Go to previous [d]iagnostic message" })
+vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Go to next [d]iagnostic message" })
+vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, { desc = "Show diagnostic [e]rror messages" })
+vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic [q]uickfix list" })
+
+vim.keymap.set("n", "<left>", "<cmd>echo \"Use h to move\"<CR>")
+vim.keymap.set("n", "<up>", "<cmd>echo \"Use k to move\"<CR>")
+vim.keymap.set("n", "<right>", "<cmd>echo \"Use l to move\"<CR>")
+vim.keymap.set("n", "<down>", "<cmd>echo \"Use j to move\"<CR>")
+
+vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
+vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
+vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
+vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
+
+-- NOTE: another helpful command :help lua-guide-autocommands
+vim.api.nvim_create_autocmd("TextYankPost", {
+  desc = "Highlight when yanking (copying) text",
+  group = vim.api.nvim_create_augroup("highlight-on-yank", { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
 
 -- Returning empty table because this file is only for base configs
 return {}
