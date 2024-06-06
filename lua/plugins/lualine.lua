ba9dae6e8359aef828c498fae0968ef044cf605a
Niccolas Maganeli <niccomaganeli@hotmail.com>
2024-06-06 15:13:53 -0300

    refactoring lualine to use opts instead of config

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,29 +1,25 @@
 return {
   "nvim-lualine/lualine.nvim",
   requires = { "nvim-tree/nvim-web-devicons" },
-  config = function()
-    local palette = require("neofusion.palette")
-
-    require("lualine").setup({
-      theme = require("neofusion.lualine"),
-      inactive_sections = {},
-      sections = {
-        lualine_a = { "mode" },
-        lualine_b = {},
-        lualine_c = { "location", "filename" },
-        lualine_x = { "diagnostics" },
-        lualine_y = { "branch", "diff" },
-        lualine_z = {
-          {
-            "filesize",
-            cond = function()
-              return vim.fn.empty(vim.fn.expand("%:t")) ~= 1
-            end,
-          },
-          { "o:encoding", fmt = string.upper },
-          { "fileformat", icons_enabled = false },
+  opts = {
+    theme = require("neofusion.lualine"),
+    inactive_sections = {},
+    sections = {
+      lualine_a = { "mode" },
+      lualine_b = {},
+      lualine_c = { "location", "filename" },
+      lualine_x = { "diagnostics" },
+      lualine_y = { "branch", "diff" },
+      lualine_z = {
+        {
+          "filesize",
+          cond = function()
+            return vim.fn.empty(vim.fn.expand("%:t")) ~= 1
+          end,
         },
+        { "o:encoding", fmt = string.upper },
+        { "fileformat", icons_enabled = false },
       },
-    })
-  end,
+    },
+  },
 }
