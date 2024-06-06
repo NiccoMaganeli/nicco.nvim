87f6a826141b0f0eaf9ec9d05151666d9a0cbec5
Niccolas Maganeli <niccomaganeli@hotmail.com>
2024-06-06 15:11:47 -0300

    custom lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,7 +1,29 @@
 return {
   "nvim-lualine/lualine.nvim",
   requires = { "nvim-tree/nvim-web-devicons" },
-  opts = {
-    theme = require("neofusion.lualine"),
-  },
+  config = function()
+    local palette = require("neofusion.palette")
+
+    require("lualine").setup({
+      theme = require("neofusion.lualine"),
+      inactive_sections = {},
+      sections = {
+        lualine_a = { "mode" },
+        lualine_b = {},
+        lualine_c = { "location", "filename" },
+        lualine_x = { "diagnostics" },
+        lualine_y = { "branch", "diff" },
+        lualine_z = {
+          {
+            "filesize",
+            cond = function()
+              return vim.fn.empty(vim.fn.expand("%:t")) ~= 1
+            end,
+          },
+          { "o:encoding", fmt = string.upper },
+          { "fileformat", icons_enabled = false },
+        },
+      },
+    })
+  end,
 }
