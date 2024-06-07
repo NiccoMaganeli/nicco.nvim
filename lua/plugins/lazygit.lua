390998c18d66e7b26362d6f5a66370324dc8dcd6
Niccolas Maganeli <niccomaganeli@hotmail.com>
2024-06-07 01:56:37 -0300

    fixing lazygit configuration

--- lua/plugins/lazygit.lua
+++ lua/plugins/lazygit.lua
@@ -13,4 +13,8 @@ return {
   keys = {
     { "<leader>lg", "<cmd>LazyGit<CR>", desc = "Open lazygit" },
   },
+  config = function()
+    vim.g.lazygit_use_custom_config_file_path = 1
+    vim.g.lazygit_config_file_path = vim.fn.stdpath("config") .. "/lazygit_config.yaml"
+  end,
 }
