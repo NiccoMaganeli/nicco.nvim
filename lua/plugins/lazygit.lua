cea25f26ed9d7d36ad9ce4a939f46ab3abea8e24
Niccolas Maganeli <niccomaganeli@hotmail.com>
2024-06-07 21:18:37 -0300

    brenda helped me debuggin lazygit

--- lua/plugins/lazygit.lua
+++ lua/plugins/lazygit.lua
@@ -14,7 +14,22 @@ return {
     { "<leader>lg", "<cmd>LazyGit<CR>", desc = "Open lazygit" },
   },
   config = function()
-    vim.g.lazygit_use_custom_config_file_path = 1
-    vim.g.lazygit_config_file_path = vim.fn.stdpath("config") .. "/lazygit_config.yaml"
+    vim.api.nvim_create_autocmd("BufWinEnter", {
+      desc = "Disable all highlights when opening Lazygit",
+      callback = function()
+        if LAZYGIT_BUFFER ~= nil then
+          vim.cmd("hi clear")
+        end
+      end,
+    })
+
+    vim.api.nvim_create_autocmd("BufWinLeave", {
+      desc = "Enable highlights when Lazygit is closed",
+      callback = function()
+        if LAZYGIT_BUFFER == nil then
+          vim.cmd("colorscheme " .. vim.g.colors_name)
+        end
+      end,
+    })
   end,
 }
