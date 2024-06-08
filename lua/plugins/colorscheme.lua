183f2c88f973cba03abfe46926164710798403b5
Niccolas Maganeli <niccomaganeli@hotmail.com>
2024-06-08 15:48:00 -0300

    don't know what happened here

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -13,21 +13,4 @@ return {
       transparency = true,
     },
   },
-  -- "diegoulloao/neofusion.nvim",
-  -- priority = 1000,
-  -- init = function()
-  --   vim.o.background = "dark"
-  --   vim.g.current_colorscheme = "neofusion"
-  --   vim.cmd.colorscheme(vim.g.current_colorscheme)
-  -- end,
-  -- opts = {
-  --   transparent_mode = true,
-  --   italic = {
-  --     comments = false,
-  --   },
-  --   palette_overrides = {
-  --     gray = "#337CA6",
-  --     dark4 = "#0E76A6",
-  --   },
-  -- },
 }
