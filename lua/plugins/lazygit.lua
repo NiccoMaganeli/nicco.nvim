bdc97d94bf7e7d4e222b22f85918197a24ce6d1c
Niccolas Maganeli <niccomaganeli@hotmail.com>
2024-06-10 02:25:32 -0300

    remove line number from lazygit buffer

--- lua/plugins/lazygit.lua
+++ lua/plugins/lazygit.lua
@@ -17,4 +17,15 @@ return {
     vim.g.lazygit_use_custom_config_file_path = 1
     vim.g.lazygit_config_file_path = vim.fn.stdpath("config") .. "/lazygit_config.yaml"
   end,
+  config = function()
+    vim.api.nvim_create_autocmd("BufWinEnter", {
+      desc = "Remove line numbers from Lazygit buffer",
+      callback = function()
+        if LAZYGIT_BUFFER ~= nil then
+          vim.api.nvim_buf_set_var(LAZYGIT_BUFFER, "number", false)
+          vim.api.nvim_buf_set_var(LAZYGIT_BUFFER, "relativenumber", false)
+        end
+      end,
+    })
+  end,
 }
