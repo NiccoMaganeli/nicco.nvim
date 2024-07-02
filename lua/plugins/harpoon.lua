17385884d600379287ec2da8cebdef98eacc11ab
Niccolas Maganeli <niccomaganeli@hotmail.com>
2024-07-01 23:57:37 -0300

    changin some harpoon configs

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -6,37 +6,68 @@ return {
     "nvim-telescope/telescope.nvim",
     "kevinhwang91/nvim-ufo",
   },
-  config = function()
-    local harpoon = require("harpoon")
-    harpoon.setup({})
-
-    vim.keymap.set("n", "<leader>a", function()
-      harpoon:list():add()
-    end, { desc = "Add current buffer to Harpoon list" })
-    vim.keymap.set("n", "<C-e>", function()
-      harpoon.ui:toggle_quick_menu(harpoon:list())
-    end, { desc = "Open Harpoon quick menu" })
-    vim.keymap.set("n", "<C-h>", function()
-      harpoon:list():select(1)
-    end, { desc = "Jump to first Harpoon file" })
-    vim.keymap.set("n", "<C-j>", function()
-      harpoon:list():select(2)
-    end, { desc = "Jump to second Harpoon file" })
-    vim.keymap.set("n", "<C-k>", function()
-      harpoon:list():select(3)
-    end, { desc = "Jump to third Harpoon file" })
-    vim.keymap.set("n", "<C-l>", function()
-      harpoon:list():select(4)
-    end, { desc = "Jump to fourth Harpoon file" })
+  opts = {},
+  keys = {
+    {
+      "<leader>a",
+      function()
+        require("harpoon"):list():add()
+      end,
+      desc = "Add current buffer to Harpoon list",
+    },
+    {
+      "<C-e>",
+      function()
+        local harpoon = require("harpoon")
+        harpoon.ui:toggle_quick_menu(harpoon:list())
+      end,
+      desc = "Open Harpoon quick menu",
+    },
+    {
+      "<C-h>",
+      function()
+        require("harpoon"):list():select(1)
+      end,
+      desc = "Jump to first Harpoon file",
+    },
+    {
+      "<C-j>",
+      function()
+        require("harpoon"):list():select(2)
+      end,
+      desc = "Jump to second Harpoon file",
+    },
+    {
+      "<C-k>",
+      function()
+        require("harpoon"):list():select(3)
+      end,
+      desc = "Jump to third Harpoon file",
+    },
+    {
+      "<C-l>",
+      function()
+        require("harpoon"):list():select(4)
+      end,
+      desc = "Jump to fourth Harpoon file",
+    },
 
     -- Toggle previous & next buffers stored within Harpoon list
-    vim.keymap.set("n", "<C-S-P>", function()
-      harpoon:list():prev()
-    end, { desc = "Toggle previous buffer in Harpoon list" })
-    vim.keymap.set("n", "<C-S-N>", function()
-      harpoon:list():next()
-    end, { desc = "Toggle next buffer in Harpoon list" })
-  end,
+    {
+      "<C-S-P>",
+      function()
+        require("harpoon"):list():prev()
+      end,
+      desc = "Toggle previous buffer in Harpoon list",
+    },
+    {
+      "<C-S-N>",
+      function()
+        require("harpoon"):list():next()
+      end,
+      desc = "Toggle next buffer in Harpoon list",
+    },
+  },
   init = function()
     require("harpoon"):extend(require("harpoon.extensions").builtins.command_on_nav("UfoEnableFold"))
   end,
